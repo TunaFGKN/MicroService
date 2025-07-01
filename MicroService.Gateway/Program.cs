582ce1362e831ad6ff3e2aad9ee7962c1dd33370
Tuna F. <swaxztn@gmail.com>
2025-07-01 10:53:45 +0300

    gateway scalar

--- MicroService.Gateway/Program.cs
+++ MicroService.Gateway/Program.cs
@@ -1,11 +1,14 @@
 using System.Text;
 using MicroService.Gateway.Options;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
+builder.Services.AddOpenApi();
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("Jwt"));
 builder.Services.AddAuthentication().AddJwtBearer(options =>
@@ -38,6 +41,8 @@ var app = builder.Build();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapReverseProxy(); // .RequireAuthorization(); Depricated 
+app.MapOpenApi();
+app.MapScalarApiReference();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
